ecd08ebabbab6a6546d38e35a2fb466f911a9eed
raihanonad <raihansetiawan3296@gmail.com>
2024-05-16 15:02:29 +0700

    schema-model-user

--- index.js
+++ index.js
@@ -2,4 +2,42 @@ require("dotenv").config();
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { GraphQLError } = require("graphql");
+const { typeDefs: typeDefsUser, resolvers: resolversUser } = require("./schema/user");
+const { database } = require("./config/mongodb");
+const { verifyToken } = require("./helpers/jwt");
 
+const server = new ApolloServer({
+    typeDefs: [typeDefsUser],
+    resolvers: [resolversUser],
+    introspection: true
+})
+
+startStandaloneServer(server, {
+    listen: {
+        port: process.env.PORT || 4000
+    },
+    context: async ({ req, res }) => {
+        return {
+            authentication: async () => {
+                const access_token = req.headers.authorization;
+
+                if (!access_token) {
+                    throw new GraphQLError("Authorization token is missing", {
+                        extensions: {
+                            code: "UNAUTHORIZED"
+                        }
+                    })
+                }
+                const token = access_token.split(" ")[1];
+                const payload = verifyToken(token);
+
+                return payload;
+            }
+        }
+    }
+}).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+})
+.catch(err => {
+    console.error(err);
+})
--- schema/user.js
+++ schema/user.js
@@ -1,10 +1,14 @@
+const { GraphQLError }  = require("graphql");
+const User = require("../models/User");
+const { hashPassword, validatePassword } = require("../helpers/bcrypt");
+const { createToken } = require("../helpers/jwt");
 
-export const typeDefs = `#graphql
+const typeDefs = `#graphql
     type User {
         _id : ID 
         email : String
         username : String
-        credit : Number
+        credit : Int
     }
 
     type Token {
@@ -34,12 +38,119 @@ export const typeDefs = `#graphql
     }
 `
 
-export const resolvers = {
+const resolvers = {
     Query : {
+            getUserById: async (_, args) => {
+            try {
+                const user = await User.findById(args.id);
+                return user;
+            } catch (error) {
+                throw error
+            }
+        },
 
+        getUserByUsername: async (_, args) => {
+            try {
+                const user = await User.findByUsername(args.username);
+                return user;
+            } catch (error) {
+                throw error
+            }
+        },
+
+        getUserByEmail: async (_, args) => {
+            try {
+                const user = await User.findByEmail(args.email);
+                return user;
+            } catch (error) {
+                throw error
+            }
+        }
     },
-    Mutation : {
 
+    Mutation : {
+        addUser: async (_, args) => {
+            try {
+                const { email, username, password } = args.newUser;
+        
+                if (!email) {
+                    throw new GraphQLError("Email is required", {
+                        extensions: {
+                            code: "BAD_REQUEST"
+                        }
+                    });
+                }
+        
+                if (!username) {
+                    throw new GraphQLError("Username is required", {
+                        extensions: {
+                            code: "BAD_REQUEST"
+                        }
+                    });
+                }
+        
+                if (!password) {
+                    throw new GraphQLError("Password is required", {
+                        extensions: {
+                            code: "BAD_REQUEST"
+                        }
+                    });
+                }
+        
+                const validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/;
+        
+                if (!email.match(validEmail)) {
+                    throw new GraphQLError("Email must be formatted (example@mail.com)", {
+                        extensions: {
+                            code: "BAD_REQUEST"
+                        }
+                    });
+                }
+        
+                if (password.length < 5) {
+                    throw new GraphQLError("Password must be at least 5 characters", {
+                        extensions: {
+                            code: "BAD_REQUEST"
+                        }
+                    });
+                }
+        
+                const userFound = await User.findByEmail(email);
+        
+                if (userFound) {
+                    throw new GraphQLError("Email already in use", {
+                        extensions: {
+                            code: "BAD_REQUEST"
+                        }
+                    });
+                }
+        
+                const user = { email, username, password };
+        
+                user.password = hashPassword(user.password);
+                console.log(user.password)
+                let result = await User.addUser(user);
+        
+                return result;
+            } catch (error) {
+                throw error;
+            }
+        },
+
+        loginUser: async (_, args) => {
+            try {
+                const token = await User.loginUser(args.login);
+                return token;
+            } catch (error) {
+                throw new GraphQLError(error.message, {
+                    extensions: {
+                        code: "UNAUTHORIZED"
+                    }
+                });
+            }
+        }
     }
 };
+
+module.exports = { typeDefs, resolvers };
 
